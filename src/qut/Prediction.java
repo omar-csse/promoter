96d2eb1383b512c471e6aacdf2388e54577f9795
omar <omar.csse@gmail.com>
2019-09-29 05:59:27 +1000

    add Prediciton.java

--- src/qut/Prediction.java
+++ src/qut/Prediction.java
@@ -1,4 +1,23 @@
 package qut;
 
+import edu.au.jacobi.pattern.Match;
+
 public class Prediction {
+
+    private final String name;
+    private final Match prediction;
+
+    Prediction(String referenceGeneName, Match prediction) {
+        this.name = referenceGeneName;
+        this.prediction = prediction;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Match getPrediction() {
+        return this.prediction;
+    }
+
 }
